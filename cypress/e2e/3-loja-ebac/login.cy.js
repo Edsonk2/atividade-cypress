99c2cf2cbaacd8ba4694e0e429c2304796a0c4ff
edsonoliveira <edsonbrasiloliveira3@gmail.com>
2025-06-05 09:35:56 -0300

    Arquivo de dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,8 +1,10 @@
 //<reference types="cypress"/>
 
+const perfil = require('../../fixtures/perfil.json')
+
 describe("Funcionalidade: Login", () => {
   beforeEach(() => {
-    cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/");
+    cy.visit("minha-conta");
   });
 
     afterEach(() => {
@@ -10,13 +12,10 @@ describe("Funcionalidade: Login", () => {
     });
 
   it("Deve fazer login com sucesso ", () => {
-    cy.get("#username").type("edson@2025.com.br");
+    cy.get("#username").type('edson@2025.com.br');
     cy.get("#password").type("edson");
     cy.get(".woocommerce-form > .button").click();
-    cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should(
-      "contain",
-      "Olá, edson-3224 (não é edson-3224? Sair)"
-    );
+    cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should("contain","Olá, edson-3224 (não é edson-3224? Sair)");
   });
 
   it("Deve exibir uma mensagen de erro ao inserir usuário inválido", () => {
@@ -36,4 +35,20 @@ describe("Funcionalidade: Login", () => {
     );
     cy.get(".woocommerce-error").should("exist");
   });
+
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get("#username").type(perfil.usuario);
+        cy.get("#password").type(perfil.senha);
+        cy.get(".woocommerce-form > .button").click();
+        cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should("contain","Olá, edson-3224 (não é edson-3224? Sair)");
+    });
+
+    it.only('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then(dados =>{
+        cy.get("#username").type(dados.usuario);
+        cy.get("#password").type(dados.senha , {log: false});
+        cy.get(".woocommerce-form > .button").click();
+        cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should("contain","Olá, edson-3224 (não é edson-3224? Sair)");
+        })
+    });
 });
