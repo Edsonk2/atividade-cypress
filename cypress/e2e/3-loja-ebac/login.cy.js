9acc51d7c6e2c41e8182cbca0d25a9a843cf521f
edsonoliveira <edsonbrasiloliveira3@gmail.com>
2025-06-05 21:44:13 -0300

    Massa de dados em lista

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -15,46 +15,44 @@ describe("Funcionalidade: Login", () => {
     cy.get("#username").type('edson@2025.com.br');
     cy.get("#password").type("edson");
     cy.get(".woocommerce-form > .button").click();
-    cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should("contain","Olá, edson-3224 (não é edson-3224? Sair)");
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
   });
 
   it("Deve exibir uma mensagen de erro ao inserir usuário inválido", () => {
     cy.get("#username").type("edson@20.com.br");
     cy.get("#password").type("edson");
     cy.get(".woocommerce-form > .button").click();
-    cy.get(".woocommerce-error").should("exist");
+    cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
   });
 
   it("Deve exibir uma mensagem de erro ao inserir senha inválida", () => {
     cy.get("#username").type("edson@2025.com.br");
     cy.get("#password").type("edsonnn");
     cy.get(".woocommerce-form > .button").click();
-    cy.get(".woocommerce-error").should(
-      "contain",
-      "Erro: A senha fornecida para o e-mail edson@2025.com.br está incorreta. Perdeu a senha?"
-    );
     cy.get(".woocommerce-error").should("exist");
+    cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail edson@2025.com.br está incorreta. Perdeu a senha?')
   });
 
     it('Deve fazer login com sucesso - Usando massa de dados', () => {
         cy.get("#username").type(perfil.usuario);
         cy.get("#password").type(perfil.senha);
         cy.get(".woocommerce-form > .button").click();
-        cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should("contain","Olá, edson-3224 (não é edson-3224? Sair)");
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
     });
 
     it('Deve fazer login com sucesso - Usando fixture', () => {
         cy.fixture('perfil').then(dados =>{
         cy.get("#username").type(dados.usuario);
-        cy.get("#password").type(dados.senha , {log: false});
+        cy.get("#password").type(dados.senha ,);
         cy.get(".woocommerce-form > .button").click();
-        cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should("contain","Olá, edson-3224 (não é edson-3224? Sair)");
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
         })
     });
 
-    it.only('Deve fazer login com sucesso comando customizado', () => {
+    it('Deve fazer login com sucesso comando customizado', () => {
         cy.login('edson@2025.com.br', 'edson')
-        cy.get(".woocommerce-MyAccount-content > :nth-child(2)").should("contain","Olá, edson-3224 (não é edson-3224? Sair)");
-       
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)').should('exist')
+        
+      
     });
 });
--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,18 +1,47 @@
 //<reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
         beforeEach(() => {
-            cy.visit('produtos')
+            produtosPage.visitarUrl()
         });
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-        //.first()
-        //.last()
-        //.eq(2)
-        .contains('Apollo Running Short')
-        .click()
-
+        produtosPage.buscarProdutosLista('Ariel Roll Sleeve Sweatshirt')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProdutos(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Zeppelin Yoga Pant')
+         cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant')
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.visitarProduto('Ariel Roll Sleeve Sweatshirt')
+        produtosPage.addprodutoCarrinho('M','green', qtd)
+
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados =>{
+        produtosPage.visitarProduto(dados[2].nomeProduto)
+        produtosPage.addprodutoCarrinho(
+            dados[2].tamanho,
+             dados[2].cor, 
+             dados[2].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+        })
+        
+        
+        
+    });
+
 });
 
