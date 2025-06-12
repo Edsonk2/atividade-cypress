e38f6be1d19e162a002ffe7e6aa5b49db939dad1
edsonoliveira <edsonbrasiloliveira3@gmail.com>
2025-06-12 20:04:50 -0300

    Primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,15 @@
+//<reference type="cypress"/>
+
+describe('Funcionalidade: Login',()=>{
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('oliveira@gmail.com.br')
+        cy.get('#password').type('edson')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, oliveira (não é oliveira? Sair)')
+
+    });
+
+
+})
