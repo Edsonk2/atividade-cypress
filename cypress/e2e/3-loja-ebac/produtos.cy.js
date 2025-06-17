1c283422fc694d334524173e44459f2aa6b68550
edsonoliveira <edsonbrasiloliveira3@gmail.com>
2025-06-17 20:50:34 -0300

    Page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,18 +1,29 @@
 //<reference type="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos-page";
+
 describe('Funcionalidade: Produtos', () => {
 
     before(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-        //.first()
-        //.last()
-        //.eq(2)
-        .contains('Ariel Roll Sleeve Sweatshirt')
-        .click()
+        produtosPage.buscarProdutoLista('Ariel Roll Sleeve Sweatshirt')
         cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+    });
+
+    it.only('Deve buscar um produto  com sucesso', () => {
+        let produto = 'Helena Hooded Fleece'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+    });
+
+    it('Deve visitar a página com sucesso', () => {
+        
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        
     });
 });
