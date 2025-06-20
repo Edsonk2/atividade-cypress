9e5bdc840a237c333f426f22cd36d26c46c7584b
edsonoliveira <edsonbrasiloliveira3@gmail.com>
2025-06-20 07:55:24 -0300

    lista cont

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -13,17 +13,21 @@ describe('Funcionalidade: Produtos', () => {
         cy.get('#tab-title-description > a').should('contain' , 'Descrição')
     });
 
-    it.only('Deve buscar um produto  com sucesso', () => {
+    it('Deve buscar um produto  com sucesso', () => {
         let produto = 'Helena Hooded Fleece'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     });
 
-    it('Deve visitar a página com sucesso', () => {
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Zeppelin Yoga pant')
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
         
     });
 
-    it('Deve adicionar produto ao carrinho', () => {
-        
+    it.only('Deve adicionar produto ao carrinho', () => {
+        produtosPage.buscarProduto('Ariel Roll Sleeve Sweatshirt')
+        produtosPage.addProdutoCarrinho('l','red',3)
+        cy.get('.woocommerce-message').should('contain','3 × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
     });
 });
--- cypress/support/page-objects/produtos-page.js
+++ cypress/support/page-objects/produtos-page.js
@@ -15,12 +15,17 @@ class produtospage{
         .click()
     }
 
-    visitarProduto(){
-
+    visitarProduto(nomeProduto){
+        //cy.visit(`produtos/${nomeProduto}`)
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
     }
 
     addProdutoCarrinho(){
-
+       cy.get('.button-variable-item-L').click()
+       cy.get('.button-variable-item-Red').click()
+       cy.get('.input-text').clear().type(3)
+       cy.get('.single_add_to_cart_button').click()
     }
 }
 
